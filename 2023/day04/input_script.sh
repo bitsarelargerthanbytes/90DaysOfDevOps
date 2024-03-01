8cd1e2f579d6a4398f5eb95519953bacb1d6a495
jordan.nistor <jordancw494@gmail.com>
2024-03-01 14:27:24 -0800

    adding changes from jnistor/day03

---
+++ 2023/day04/input_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "Enter your name: "
+
+read userName
+
+argCount=$#
+arg1=$1
+arg=$2
+
+
+echo "User Input: $username"
+echo "Number of command-line arguments: $argCount"
+echo "Argument 1: $arg1"
+echo "Argument 2: arg2"
